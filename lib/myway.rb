d41315e04c29fcb30930d2948ca71ab926c99052
Fumitaka Tokumitsu <toku345@gmail.com>
2014-01-03 20:37:32 +0900

    Add Simple File Uploader

--- lib/myway.rb
+++ lib/myway.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'sinatra/base'
 require 'ostruct'
 require 'time'
@@ -6,6 +7,32 @@ class MyWay < Sinatra::Base
   set :root, File.expand_path('../../', __FILE__)
 
   get '/' do
-    erb :index
+    files = [
+      {
+        name: 'sample.jpg',
+        updated: Date.parse('2014/01/01 12:00'),
+        file_type: 'jpg',
+        file_url: '/'
+      }
+    ]
+    erb :index, :locals => { files: files }
+  end
+
+  get '/upload' do
+    erb :upload
+  end
+
+  post '/upload' do
+    if params[:file]
+      upload_path = "./upload/#{params[:file][:filename]}"
+      File.open(upload_path, "wb") do |f|
+        f.write params[:file][:tempfile].read
+        @result = "アップロード成功"
+      end
+    else
+      @result = "アップロード失敗"
+    end
+
+    erb :upload
   end
 end
